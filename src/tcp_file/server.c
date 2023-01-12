919325ace9192ed0a89e5e9687f022294b751ea0
yunkai1841 <70383706+yunkai1841@users.noreply.github.com>
2023-01-13 00:43:17 +0900

    clone tcp/

---
+++ src/tcp_file/server.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define PORT 8000
+
+int main()
+{
+    int sockfd, new_sockfd;
+    socklen_t clilen;
+    char buffer[256];
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
+
+    // ソケットを作成する
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("ERROR opening socket");
+        exit(1);
+    }
+
+    // ソケットにアドレスを割り当てる
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(PORT);
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        perror("ERROR on binding");
+        exit(1);
+    }
+
+    // クライアントからの接続を待つ
+    listen(sockfd, 5);
+    clilen = sizeof(cli_addr);
+    new_sockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+    if (new_sockfd < 0) {
+        perror("ERROR on accept");
+        exit(1);
+    }
+
+    // データを受信する
+    bzero(buffer, 256);
+    n = read(new_sockfd, buffer, 255);
+    if (n < 0) {
+        perror("ERROR reading from socket");
+        exit(1);
+    }
+
+    printf("Message from client: %s\n", buffer);
+
+    // データを送信する
+    n = write(new_sockfd, "I got your message", 18);
+    if (n < 0) {
+        perror("ERROR writing to socket");
+        exit(1);
+    }
+
+    close(new_sockfd);
+    close(sockfd);
+
+    return 0;
+}
