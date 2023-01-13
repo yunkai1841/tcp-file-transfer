38ba20448756e8aea70471832a2f519f5fb210dd
yunkai1841 <70383706+yunkai1841@users.noreply.github.com>
2023-01-13 13:21:06 +0900

    remove directory from list

--- src/util/filelist.c
+++ src/util/filelist.c
@@ -1,6 +1,7 @@
 #include <dirent.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 
 #define MAX_FILES 1000
@@ -8,6 +9,7 @@
 int filelist(char *directory, char file_list[MAX_FILES][NAME_MAX + 1]) {
     DIR *dir;
     struct dirent *entry;
+    struct stat stat_buf;
     int file_count = 0;
 
     if ((dir = opendir(directory)) == NULL) {
@@ -18,6 +20,13 @@ int filelist(char *directory, char file_list[MAX_FILES][NAME_MAX + 1]) {
     while ((entry = readdir(dir)) != NULL) {
         if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
             continue;
+        char path[PATH_MAX];
+        snprintf(path, PATH_MAX, "%s/%s", directory, entry->d_name);
+        if (stat(path, &stat_buf) == 0) {
+            if (S_ISDIR(stat_buf.st_mode)) {
+                continue;
+            }
+        }
         strcpy(file_list[file_count], entry->d_name);
         file_count++;
     }
@@ -32,13 +41,4 @@ void print_filelist(char *directory) {
     for (int i = 0; i < file_count; i++) {
         printf("%s\n", file_list[i]);
     }
-}
-
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <directory>\n", argv[0]);
-        return 1;
-    }
-    print_filelist(argv[1]);
-    return 0;
 }
