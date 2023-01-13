ad4e68a234a6b6ad4b87b433edcd00d23e470c87
yunkai1841 <70383706+yunkai1841@users.noreply.github.com>
2023-01-13 12:41:39 +0900

    implement filelist getter

---
+++ src/util/filelist.c
@@ -0,0 +1,23 @@
+#include <dirent.h>
+#include <stdio.h>
+#include <sys/types.h>
+
+int main(int argc, char *argv[]) {
+    DIR *dir;
+    struct dirent *entry;
+    char *directory = argv[1];
+
+    if ((dir = opendir(directory)) == NULL) {
+        printf("Cannot open directory: %s\n", directory);
+        return 1;
+    }
+
+    while ((entry = readdir(dir)) != NULL) {
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+            continue;
+        printf("%s\n", entry->d_name);
+    }
+
+    closedir(dir);
+    return 0;
+}
